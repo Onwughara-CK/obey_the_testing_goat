24322eb9c3112b2828d480198ab09f2d7a1d382c
Collins <kelechicollins.93@gmail.com>
2020-08-16 20:08:43 +0100

    use unittest to handle test

--- functional_test.py
+++ functional_test.py
@@ -1,7 +1,21 @@
+import unittest
 from selenium import webdriver
 
-driver = webdriver.Chrome(executable_path='./../chromedriver.exe')
 
-driver.get('http://localhost:8000')
+class NewVisitorTest(unittest.TestCase):
+    def setUp(self):
+        self.driver = webdriver.Chrome(executable_path='./../chromedriver.exe')
 
-assert "Django" in driver.title
+    def tearDown(self):
+        self.driver.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        self.driver.get('http://localhost:8000')
+
+        self.assertIn('To-Do', self.driver.title)
+
+        self.fail('finish the test')
+
+
+if __name__ == "__main__":
+    unittest.main()
