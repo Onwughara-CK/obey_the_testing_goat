4d171b2f264f6ccf2733c6322a0fd3ce12883d91
Collins <kelechicollins.93@gmail.com>
2020-08-16 16:43:23 +0100

    use assert to check browser title

--- functional_test.py
+++ functional_test.py
@@ -1,5 +1,7 @@
 from selenium import webdriver
 
-driver = webdriver.Chrome(executable_path='./chromedriver.exe')
+driver = webdriver.Chrome(executable_path='./../chromedriver.exe')
 
 driver.get('http://localhost:8000')
+
+assert "Django" in driver.title
