eaedc4203acb9b9ea461c9970e79a10a53e622ce
Collins <kelechicollins.93@gmail.com>
2020-08-17 22:02:41 +0100

    hoe_page view now return html

--- lists/tests.py
+++ lists/tests.py
@@ -1,9 +1,17 @@
 from django.test import TestCase
 from django.urls import resolve
-
+from django.http import HttpRequest
 from .views import home_page
 
 
 class HomePageTest(TestCase):
     def test_root_url_resolves_to_home_page_view(self):
         self.assertEqual(resolve("/").func, home_page)
+
+    def test_home_page_returns_correct_html(self):
+        request = HttpRequest()
+        response = home_page(request)
+        html = response.content.decode('utf8')
+        self.assertTrue(html.startswith('<html>'))
+        self.assertIn('<title>To-Do lists</title>', html)
+        self.assertTrue(html.endswith('<html>'))
